733ff74668da4e0aa9d699aa3c5ae1749a944560
rajgupta89 <gupta77raj123@gmail.com>
2022-03-06 11:58:36 +0530

    commit

---
+++ hms/include/footer.php
@@ -0,0 +1,10 @@
+<footer>
+				<div class="footer-inner">
+					<div class="pull-left">
+						&copy; <span class="current-year"></span><span class="text-bold text-uppercase"> E-HS</span>. <span>Created by Raj Gupta. All rights reserved</span>
+					</div>
+					<div class="pull-right">
+						<span class="go-top"><i class="ti-angle-up"></i></span>
+					</div>
+				</div>
+			</footer>
